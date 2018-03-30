6d2208b592e95ee6da43a5c78d42f2355ae07403
Sotoam_Bak_s3542733 <s3542733@student.rmit.edu.au>
2018-03-30 18:31:30 +1100

    Progress Update...
    Implement most of the functionality for the first part of the java portion of the assingment. I've written a part of the second part so that I can test the readibility of heap file. I've commented that out but I can confirm that it works. Running the code on the full dataset takes several seconds, but I have confirmed that it works fine.

--- DBLoad.java
+++ DBLoad.java
@@ -1,11 +1,13 @@
 package dbs_a1_heapfile;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.StringTokenizer;
@@ -17,6 +19,7 @@ public class DBLoad {
 		int heapPageSize;
 		int recordsPerPage;
 		int recordID = 0;
+		int pageNum = 0;
 		int sizeOfRecord = 4 + 341;
 		String dataFileName;
 		String heapFileName;
@@ -28,11 +31,10 @@ public class DBLoad {
 		StringTokenizer	strTok;
 		
 		if(true) {
-			
-			heapPageSize = Integer.parseInt(args[1]);	
+			heapPageSize = Integer.parseInt(args[1]);
 			dataFileName = args[2];
 			heapFileName = "heap." + args[1];
-			recordsPerPage = heapPageSize/sizeOfRecord;
+			recordsPerPage = (heapPageSize-4)/sizeOfRecord;
 			
 			if(recordsPerPage >= 1) {
 				try {
@@ -40,71 +42,86 @@ public class DBLoad {
 					heapFile = new FileOutputStream(new File(heapFileName));
 					lineRead = dataFile.readLine();
 					while(lineRead != null) {
-						String[] tokArr = lineRead.split("\t", -1);
-						byte[] record = new byte[sizeOfRecord];
-						byte[] rid = ByteBuffer.allocate(4).putInt(recordID).array();
-						System.arraycopy(rid, 0, record, 0, rid.length);
-						byte[] registerName = tokArr[0].getBytes("utf-8");
-						byte[] REGISTER_NAME = new byte[14];
-						if (tokArr[0] != "") {
-							System.arraycopy(registerName, 0, REGISTER_NAME, 0, registerName.length);
+						int recordCounter = 0;
+						int currOffset = 0;
+						byte[] heapPage = new byte[heapPageSize];
+						ByteBuffer pageNumBytes = ByteBuffer.allocate(4);
+						pageNumBytes.putInt(pageNum);
+						System.arraycopy(pageNumBytes.array(), 0, heapPage, heapPage.length-4, pageNumBytes.array().length);
+						while(recordCounter != recordsPerPage) {
+							if(lineRead == null ) {
+								break;
+							}
+							String[] tokArr = lineRead.split("\t", -1);
+							byte[] record = new byte[sizeOfRecord];
+							byte[] rid = ByteBuffer.allocate(4).putInt(recordID).array();
+							System.arraycopy(rid, 0, record, 0, rid.length);
+							byte[] registerName = tokArr[0].getBytes("utf-8");
+							byte[] REGISTER_NAME = new byte[14];
+							if (tokArr[0] != "") {
+								System.arraycopy(registerName, 0, REGISTER_NAME, 0, registerName.length);
+							}
+							System.arraycopy(REGISTER_NAME, 0, record, 4, REGISTER_NAME.length);
+							byte[] bnName = tokArr[1].getBytes();
+							byte[] BN_NAME = new byte[256];
+							if (tokArr[1] != "") {
+								System.arraycopy(bnName, 0, BN_NAME, 0, bnName.length);
+							}
+							System.arraycopy(BN_NAME, 0, record, 18, BN_NAME.length);
+							byte[] bnStatus = tokArr[2].getBytes();
+							byte[] BN_STATUS = new byte[12];
+							if (tokArr[2] != "") {
+								System.arraycopy(bnStatus, 0, BN_STATUS, 0, bnStatus.length);
+							}
+							System.arraycopy(BN_STATUS, 0, record, 274, BN_STATUS.length);
+							byte[] bnRegDt = tokArr[3].getBytes();
+							byte[] BN_REG_DT = new byte[10];
+							if (tokArr[3] != "") {
+								System.arraycopy(bnRegDt, 0, BN_REG_DT, 0, bnRegDt.length);
+							}
+							System.arraycopy(BN_REG_DT, 0, record, 286, BN_REG_DT.length);
+							byte[] bnCancelDt = tokArr[4].getBytes();
+							byte[] BN_CANCEL_DT = new byte[10];
+							if (tokArr[4] != "") {
+								System.arraycopy(bnCancelDt, 0, BN_CANCEL_DT, 0, bnCancelDt.length);
+							}
+							System.arraycopy(BN_CANCEL_DT, 0, record, 296, BN_CANCEL_DT.length);
+							byte[] bnRenewDt = tokArr[5].getBytes();
+							byte[] BN_RENEW_DT = new byte[10];
+							if (tokArr[5] != "") {
+								System.arraycopy(bnRenewDt, 0, BN_RENEW_DT, 0, bnRenewDt.length);
+							}
+							System.arraycopy(BN_RENEW_DT, 0, record, 306, BN_RENEW_DT.length);
+							byte[] bnStateNum = tokArr[6].getBytes();
+							byte[] BN_STATE_NUM = new byte[12];
+							if (tokArr[6] != "") {
+								System.arraycopy(bnStateNum, 0, BN_STATE_NUM, 0, bnStateNum.length);
+							}
+							System.arraycopy(BN_STATE_NUM, 0, record, 316, BN_STATE_NUM.length);
+							byte[] bnStateOfReg = tokArr[7].getBytes();
+							byte[] BN_STATE_OF_REG = new byte[3];
+							if (tokArr[7] != "") {
+								System.arraycopy(bnStateOfReg, 0, BN_STATE_OF_REG, 0, bnStateOfReg.length);
+							}
+							System.arraycopy(BN_STATE_OF_REG, 0, record, 328, BN_STATE_OF_REG.length);
+							byte[] bnAbn = tokArr[8].getBytes();
+							byte[] BN_ABN = new byte[14];
+							if (tokArr[8] != "") {
+								System.arraycopy(bnAbn, 0, BN_ABN, 0, bnAbn.length);
+							}
+							System.arraycopy(BN_ABN, 0, record, 331, BN_ABN.length);
+							System.arraycopy(record, 0, heapPage, currOffset, record.length);
+							currOffset = currOffset + sizeOfRecord;
+							recordCounter++;
+							recordID++;
+							System.out.println(recordID);
+							lineRead = dataFile.readLine();
 						}
-						System.arraycopy(REGISTER_NAME, 0, record, 4, REGISTER_NAME.length);
-						byte[] bnName = tokArr[1].getBytes();
-						byte[] BN_NAME = new byte[256];
-						if (tokArr[1] != "") {
-							System.arraycopy(bnName, 0, BN_NAME, 0, bnName.length);
-						}
-						System.arraycopy(BN_NAME, 0, record, 18, BN_NAME.length);
-						byte[] bnStatus = tokArr[2].getBytes();
-						byte[] BN_STATUS = new byte[12];
-						if (tokArr[2] != "") {
-							System.arraycopy(bnStatus, 0, BN_STATUS, 0, bnStatus.length);
-						}
-						System.arraycopy(BN_STATUS, 0, record, 274, BN_STATUS.length);
-						byte[] bnRegDt = tokArr[3].getBytes();
-						byte[] BN_REG_DT = new byte[10];
-						if (tokArr[3] != "") {
-							System.arraycopy(bnRegDt, 0, BN_REG_DT, 0, bnRegDt.length);
-						}
-						System.arraycopy(BN_REG_DT, 0, record, 286, BN_REG_DT.length);
-						byte[] bnCancelDt = tokArr[4].getBytes();
-						byte[] BN_CANCEL_DT = new byte[10];
-						if (tokArr[4] != "") {
-							System.arraycopy(bnCancelDt, 0, BN_CANCEL_DT, 0, bnCancelDt.length);
-						}
-						System.arraycopy(BN_CANCEL_DT, 0, record, 296, BN_CANCEL_DT.length);
-						byte[] bnRenewDt = tokArr[5].getBytes();
-						byte[] BN_RENEW_DT = new byte[10];
-						if (tokArr[5] != "") {
-							System.arraycopy(bnRenewDt, 0, BN_RENEW_DT, 0, bnRenewDt.length);
-						}
-						System.arraycopy(BN_RENEW_DT, 0, record, 306, BN_RENEW_DT.length);
-						byte[] bnStateNum = tokArr[6].getBytes();
-						byte[] BN_STATE_NUM = new byte[12];
-						if (tokArr[6] != "") {
-							System.arraycopy(bnStateNum, 0, BN_STATE_NUM, 0, bnStateNum.length);
-						}
-						System.arraycopy(BN_STATE_NUM, 0, record, 316, BN_STATE_NUM.length);
-						byte[] bnStateOfReg = tokArr[7].getBytes();
-						byte[] BN_STATE_OF_REG = new byte[3];
-						if (tokArr[7] != "") {
-							System.arraycopy(bnStateOfReg, 0, BN_STATE_OF_REG, 0, bnStateOfReg.length);
-						}
-						System.arraycopy(BN_STATE_OF_REG, 0, record, 328, BN_STATE_OF_REG.length);
-						byte[] bnAbn = tokArr[8].getBytes();
-						byte[] BN_ABN = new byte[14];
-						if (tokArr[8] != "") {
-							System.arraycopy(bnAbn, 0, BN_ABN, 0, bnAbn.length);
-						}
-						System.arraycopy(BN_ABN, 0, record, 331, BN_ABN.length);
-						heapFile.write(record);
-						
-						lineRead = dataFile.readLine();
-						recordID++;
-						
+						heapFile.write(heapPage);
+						pageNum++;
 					}
-					
+					System.out.println(recordID + " records created.");
+					System.out.println(pageNum + " pages created.");
 					heapFile.close();
 					
 				} catch(FileNotFoundException e) {
@@ -113,22 +130,33 @@ public class DBLoad {
 					e.printStackTrace();
 				}
 			}
-			try {
+			/*try {
 				FileInputStream readIn = new FileInputStream(new File(heapFileName));
-				while (readIn.read() != -1) {
-					byte[] line = new byte[345];
-					readIn.read(line, 0, 345);
-					byte[] ridBytes = new byte[4];
-					ByteBuffer ridWrap = ByteBuffer.wrap(ridBytes);
-					System.arraycopy(line, 0, ridBytes, 0, 4);
-					int rid = ridWrap.getInt();
-					System.out.write(line);
+				BufferedWriter wo = new BufferedWriter(new FileWriter("test.txt"));
+				int sizeOfPage = 4096;
+				int pageOffset = 0;
+				while(readIn.available() != 0) {
+					byte[] page = new byte[heapPageSize];
+					readIn.read(page, 0, sizeOfPage);
+					int recordOffset = 0;
+					for(int i=0; i < recordsPerPage; i++) {
+						byte[] line = new byte[345];
+						System.arraycopy(page, recordOffset, line, 0, sizeOfRecord);
+						byte[] ridBytes = new byte[4];
+						ByteBuffer ridWrap = ByteBuffer.wrap(ridBytes);
+						System.arraycopy(line, 0, ridBytes, 0, 4);
+						int rid = ridWrap.getInt();
+						byte[] recordBytes = new byte[341];
+						System.arraycopy(line, 4, recordBytes, 0, 341);
+						System.out.println(rid + " " + new String(recordBytes));
+						recordOffset += sizeOfRecord;
+					}
 				}
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
-			}
+			}*/
 		}
 	}
 }
